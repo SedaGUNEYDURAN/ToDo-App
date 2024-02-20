3782d2b71f82fd6c25c32df796f148f9e1dad3fa
SedaGUNEYDURAN <seda2guney@gmail.com>
2024-02-20 17:24:38 +0300

    LoginController

---
+++ src/main/java/com/toDoApp/login/LoginController.java
@@ -0,0 +1,11 @@
+package com.toDoApp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class LoginController {
+	@RequestMapping("login")
+	public String goToLoginPage() {
+		return "login";
+	}
+}
--- target/classes/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+++ target/classes/META-INF/resources/WEB-INF/jsp/sayHello.jsp
@@ -3,6 +3,8 @@
 		<title>My first HTML Page </title>
 	</head>
 	<body>
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
 		My first html page with body 
 	</body>
 </html>
