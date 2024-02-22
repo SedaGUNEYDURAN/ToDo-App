c74bd26f938e9896bedd249db3a8956dd23201fd
SedaGUNEYDURAN <seda2guney@gmail.com>
2024-02-22 13:33:26 +0300

    Login Form

--- src/main/java/com/toDoApp/login/LoginController.java
+++ src/main/java/com/toDoApp/login/LoginController.java
@@ -1,21 +1,15 @@
 package com.toDoApp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
-	private Logger logger=LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
-	public String goToLoginPage(@RequestParam String name, ModelMap model) {
-		model.put("name",name);
-		logger.debug("Request param is {}",name);  //log level=debug olursa basiliyor; info olursa basilmiyor
-		logger.info("I want this printed at info level");
-		logger.warn("I want this printed at info level");
-		System.out.println("name:"+name);
+	public String goToLoginPage() {
+		
 		return "login";
 	}
 }
