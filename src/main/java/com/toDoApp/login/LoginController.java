e48f093eeb7632da37cfa807bfdbc8b458e0d7e1
SedaGUNEYDURAN <seda2guney@gmail.com>
2024-05-24 15:10:06 +0300

    security

--- src/main/java/com/toDoApp/login/LoginController.java
+++ src/main/java/com/toDoApp/login/LoginController.java
@@ -10,29 +10,30 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @Controller
 //@SessionAttributes("name")
 public class LoginController {
-	private AuthenticationService authenticationService;
-	
-	public LoginController(AuthenticationService authenticationService) {
-		super();
-		this.authenticationService = authenticationService;
-	}
+//	private AuthenticationService authenticationService;
+//	
+//	public LoginController(AuthenticationService authenticationService) {
+//		super();
+//		this.authenticationService = authenticationService;
+//	}
 
-	@RequestMapping(value="login", method=RequestMethod.GET)
-	public String goToLoginPage() {
+	@RequestMapping(value="/", method=RequestMethod.GET)//login
+	public String goToLoginPage(ModelMap model) {
+		model.put("name","seda"); 
 		
-		return "login";
+		return "welcome";//login
 	}
 	
-	@RequestMapping(value="login", method=RequestMethod.POST)
-	public String goToWelcomePage(@RequestParam String name,@RequestParam String password, ModelMap model) {
-		if(authenticationService.authenticate(name, password)) {
-			model.put("name",name);
-			model.put("password",password);
-			return "welcome";
-		}
-		model.put("errorMessage", "Invalid Creditials! Please try again");
-		return "login"; 
-		
-		
-	}
+//	@RequestMapping(value="login", method=RequestMethod.POST)
+//	public String goToWelcomePage(@RequestParam String name,@RequestParam String password, ModelMap model) {
+//		if(authenticationService.authenticate(name, password)) {
+//			model.put("name",name);
+//			model.put("password",password);
+//			return "welcome";
+//		}
+//		model.put("errorMessage", "Invalid Creditials! Please try again");
+//		return "login"; 
+//		
+//		
+//	}
 }
