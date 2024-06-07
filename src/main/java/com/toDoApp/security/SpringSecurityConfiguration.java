1ea5dee4ffc4bcb969503cb469536f3e73b71565
SedaGUNEYDURAN <seda2guney@gmail.com>
2024-06-07 16:50:33 +0300

    Password Encoder

---
+++ src/main/java/com/toDoApp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.toDoApp.security;
+
+import java.util.function.Function;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SpringSecurityConfiguration {
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		Function<String, String> passwordEncoder=input->passwordEncoder().encode(input);
+		UserDetails userDetails=User.builder()
+		.passwordEncoder(passwordEncoder)
+		.username("Seda")
+		.password("1234")
+		.roles("USER","ADMIN")
+		.build();
+		return new InMemoryUserDetailsManager(userDetails);
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
